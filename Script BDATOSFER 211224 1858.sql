1d00d00fbd1c9a4a5b22111da2fcb7240fbf561e
Terry Stifler Ramírez <terryman42@gmail.com>
2024-12-26 23:08:23 -0600

    Update Script BDATOSFER 211224 1858.sql

--- Script BDATOSFER 211224 1858.sql
+++ Script BDATOSFER 211224 1858.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `bdatosfer` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `bdatosfer`;
 -- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
 --
 -- Host: localhost    Database: bdatosfer
@@ -9,13 +7,21 @@ USE `bdatosfer`;
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `bdatosfer`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `bdatosfer` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `bdatosfer`;
 
 --
 -- Table structure for table `categoria`
@@ -61,7 +67,7 @@ CREATE TABLE `cliente` (
   PRIMARY KEY (`IdCliente`),
   UNIQUE KEY `Documento` (`Documento`),
   UNIQUE KEY `Correo` (`Correo`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -70,7 +76,7 @@ CREATE TABLE `cliente` (
 
 LOCK TABLES `cliente` WRITE;
 /*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
-INSERT INTO `cliente` VALUES (2,'CF-000','Consumidor Final','consumidor.final@nulo.com','0000-0000',1,'2024-12-13 23:12:59'),(3,'C001','Juan Pérez','juan.perez@example.com','505-8888-1234',1,'2024-12-13 23:12:59'),(4,'C002','María López','maria.lopez@example.com','505-8899-5678',1,'2024-12-13 23:12:59'),(5,'C003','Carlos Rodríguez','carlos.rodriguez@example.com','505-8777-4321',1,'2024-12-13 23:12:59'),(6,'C004','Ana Castillo','ana.castillo@example.com','505-8866-3456',1,'2024-12-13 23:12:59'),(7,'C005','Luis Gómez','luis.gomez@example.com','505-8999-6543',1,'2024-12-13 23:12:59'),(8,'C006','Sofía Vargas','sofia.vargas@example.com','505-8333-6789',1,'2024-12-13 23:12:59'),(9,'C007','José Hernández','jose.hernandez@example.com','505-8555-9876',1,'2024-12-13 23:12:59'),(10,'C008','Gloria Morales','gloria.morales@example.com','505-8222-3456',1,'2024-12-13 23:12:59'),(11,'C009','Miguel Rivas','miguel.rivas@example.com','505-8777-5678',1,'2024-12-13 23:12:59'),(12,'C010','Paola Martínez','paola.martinez@example.com','505-8666-7890',1,'2024-12-13 23:12:59'),(13,'201271292003L','Terry Ramirez','terryman42@gmail.com','85584910',1,'2024-12-14 22:00:52');
+INSERT INTO `cliente` VALUES (2,'CF-000','Consumidor Final','consumidor.final@nulo.com','0000-0000',1,'2024-12-13 23:12:59'),(3,'C001','Juan Pérez','juan.perez@example.com','505-8888-1234',1,'2024-12-13 23:12:59'),(4,'C002','María López','maria.lopez@example.com','505-8899-5678',1,'2024-12-13 23:12:59'),(5,'C003','Carlos Rodríguez','carlos.rodriguez@example.com','505-8777-4321',1,'2024-12-13 23:12:59'),(6,'C004','Ana Castillo','ana.castillo@example.com','505-8866-3456',1,'2024-12-13 23:12:59'),(7,'C005','Luis Gómez','luis.gomez@example.com','505-8999-6543',1,'2024-12-13 23:12:59'),(8,'C006','Sofía Vargas','sofia.vargas@example.com','505-8333-6789',1,'2024-12-13 23:12:59'),(9,'C007','José Hernández','jose.hernandez@example.com','505-8555-9876',1,'2024-12-13 23:12:59'),(10,'C008','Gloria Morales','gloria.morales@example.com','505-8222-3456',1,'2024-12-13 23:12:59'),(11,'C009','Miguel Rivas','miguel.rivas@example.com','505-8777-5678',1,'2024-12-13 23:12:59'),(12,'C010','Paola Martínez','paola.martinez@example.com','505-8666-7890',1,'2024-12-13 23:12:59'),(13,'201271292003L','Terry Ramirez','terryman42@gmail.com','85584910',1,'2024-12-14 22:00:52'),(14,'asdfasdf','asdfadf','asdf@gmail.com','34345',1,'2024-12-26 18:54:55');
 /*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -92,7 +98,7 @@ CREATE TABLE `compra` (
   KEY `fk_compra_proveedor` (`IdProveedor`),
   CONSTRAINT `fk_compra_proveedor` FOREIGN KEY (`IdProveedor`) REFERENCES `proveedor` (`IdProveedor`) ON DELETE SET NULL,
   CONSTRAINT `fk_compra_usuario` FOREIGN KEY (`IdUsuario`) REFERENCES `usuario` (`IdUsuario`) ON DELETE SET NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -101,6 +107,7 @@ CREATE TABLE `compra` (
 
 LOCK TABLES `compra` WRITE;
 /*!40000 ALTER TABLE `compra` DISABLE KEYS */;
+INSERT INTO `compra` VALUES (1,9,NULL,50.00,'2024-12-26 17:30:41'),(2,9,1,60.00,'2024-12-26 17:34:41'),(3,9,4,60.00,'2024-12-26 17:37:17'),(4,9,5,540.00,'2024-12-26 17:37:56'),(5,5,11,60.00,'2024-12-26 17:41:49'),(6,5,1,5.00,'2024-12-26 20:00:58');
 /*!40000 ALTER TABLE `compra` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -124,7 +131,7 @@ CREATE TABLE `detalle_compra` (
   KEY `fk_detalle_compra_producto` (`IdProducto`),
   CONSTRAINT `fk_detalle_compra_compra` FOREIGN KEY (`IdCompra`) REFERENCES `compra` (`IdCompra`) ON DELETE CASCADE,
   CONSTRAINT `fk_detalle_compra_producto` FOREIGN KEY (`IdProducto`) REFERENCES `producto` (`IdProducto`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -133,6 +140,7 @@ CREATE TABLE `detalle_compra` (
 
 LOCK TABLES `detalle_compra` WRITE;
 /*!40000 ALTER TABLE `detalle_compra` DISABLE KEYS */;
+INSERT INTO `detalle_compra` VALUES (1,1,31,0.00,5.00,10,50.00),(2,NULL,31,0.00,5.00,12,60.00),(3,2,31,0.00,5.00,12,60.00),(4,3,31,0.00,5.00,12,60.00),(5,4,31,0.00,5.00,8,40.00),(6,4,34,0.00,50.00,10,500.00),(7,5,40,12.00,0.00,5,60.00),(8,6,32,2.50,4.00,2,5.00);
 /*!40000 ALTER TABLE `detalle_compra` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -155,7 +163,7 @@ CREATE TABLE `detalle_venta` (
   KEY `fk_detalle_venta_producto` (`IdProducto`),
   CONSTRAINT `fk_detalle_venta_producto` FOREIGN KEY (`IdProducto`) REFERENCES `producto` (`IdProducto`),
   CONSTRAINT `fk_detalle_venta_venta` FOREIGN KEY (`IdVenta`) REFERENCES `venta` (`IdVenta`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -164,6 +172,7 @@ CREATE TABLE `detalle_venta` (
 
 LOCK TABLES `detalle_venta` WRITE;
 /*!40000 ALTER TABLE `detalle_venta` DISABLE KEYS */;
+INSERT INTO `detalle_venta` VALUES (2,2,36,12.00,20,240.00),(3,3,37,5.50,50,275.00);
 /*!40000 ALTER TABLE `detalle_venta` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -225,7 +234,7 @@ CREATE TABLE `producto` (
 
 LOCK TABLES `producto` WRITE;
 /*!40000 ALTER TABLE `producto` DISABLE KEYS */;
-INSERT INTO `producto` VALUES (31,'PROD001','Martillo de Uña','Martillo de acero con mango de madera',1,50,5.00,8.00,1,'2024-12-13 23:08:10',8),(32,'PROD002','Destornillador Plano','Destornillador de punta plana',2,100,2.50,4.00,1,'2024-12-13 23:08:10',8),(33,'PROD003','Llave Inglesa','Llave ajustable de 8 pulgadas',3,30,10.00,15.00,1,'2024-12-13 23:08:10',8),(34,'PROD004','Taladro Eléctrico','Taladro de 500W con brocas incluidas',4,20,50.00,75.00,1,'2024-12-13 23:08:10',18),(35,'PROD005','Cinta Métrica','Cinta métrica de 5 metros',5,70,3.00,5.00,1,'2024-12-13 23:08:10',1),(36,'PROD006','Sierra Manual','Sierra de mano para madera',6,40,8.00,12.00,1,'2024-12-13 23:08:10',8),(37,'PROD007','Tornillos 1 pulgada','Paquete de 100 tornillos de acero',7,150,3.50,5.50,1,'2024-12-13 23:08:10',9),(38,'PROD008','Clavos de 2 pulgadas','Paquete de 200 clavos galvanizados',7,200,4.00,6.50,1,'2024-12-13 23:08:10',9),(39,'PROD009','Pegamento Epóxico','Resina epóxica de 50 ml',8,60,2.00,3.50,1,'2024-12-13 23:08:10',5),(40,'PROD010','Pintura Blanca','Galón de pintura blanca mate',9,25,12.00,18.00,1,'2024-12-13 23:08:10',12),(41,'PROD011','Brocha 2 pulgadas','Brocha para pintar superficies lisas',9,80,1.50,3.00,1,'2024-12-13 23:08:10',8),(42,'PROD012','Rodillo de Pintura','Rodillo para pintura de paredes',9,50,4.00,6.50,1,'2024-12-13 23:08:10',8),(43,'PROD013','Cemento Portland','Bolsa de cemento de 50 kg',10,100,6.00,10.00,1,'2024-12-13 23:08:10',6),(44,'PROD014','Arena Fina','Metro cúbico de arena fina',10,20,15.00,25.00,1,'2024-12-13 23:08:10',14),(45,'PROD015','Tubería PVC 1 pulgada','Tubería de PVC para agua',11,60,1.20,2.00,1,'2024-12-13 23:08:10',1),(46,'PROD016','Codo PVC 90°','Codo de PVC para unión de tuberías',11,100,0.50,1.00,1,'2024-12-13 23:08:10',8),(47,'PROD017','Lija Grano Fino','Hoja de lija de 200 granos',12,200,0.30,0.60,1,'2024-12-13 23:08:10',8),(48,'PROD018','Masilla Plástica','Bote de masilla para reparación',12,50,3.00,5.00,1,'2024-12-13 23:08:10',5),(49,'PROD019','Cerradura de Puerta','Cerradura con llave y perilla',13,30,12.00,20.00,1,'2024-12-13 23:08:10',8),(50,'PROD020','Bisagra de Acero','Juego de bisagras para puertas',13,70,2.50,4.50,1,'2024-12-13 23:08:10',8),(51,'PROD021','Cable Eléctrico 2x16','Rollo de 100 metros',14,10,25.00,35.00,1,'2024-12-13 23:08:10',1),(52,'PROD022','Interruptor Unipolar','Interruptor eléctrico de pared',14,80,1.50,2.50,1,'2024-12-13 23:08:10',8),(53,'PROD023','Bombillo LED 9W','Bombillo LED de luz blanca',15,100,2.00,3.50,1,'2024-12-13 23:08:10',8),(54,'PROD024','Extensión Eléctrica','Extensión de 5 metros',15,50,6.00,10.00,1,'2024-12-13 23:08:10',1),(55,'PROD025','Guantes de Trabajo','Guantes de seguridad talla M',16,60,2.50,4.00,1,'2024-12-13 23:08:10',8),(56,'PROD026','Casco de Seguridad','Casco de seguridad para construcción',16,20,8.00,12.00,1,'2024-12-13 23:08:10',8),(57,'PROD027','Mascarilla Antipolvo','Paquete de 10 mascarillas',16,100,1.50,3.00,1,'2024-12-13 23:08:10',8),(58,'PROD028','Escalera de Aluminio','Escalera de 6 peldaños',17,15,35.00,50.00,1,'2024-12-13 23:08:10',1),(59,'PROD029','Carretilla','Carretilla metálica reforzada',17,5,40.00,65.00,1,'2024-12-13 23:08:10',1),(60,'PROD030','Nivel de Burbuja','Nivel de burbuja de 60 cm',18,30,6.00,9.00,1,'2024-12-13 23:08:10',8);
+INSERT INTO `producto` VALUES (31,'PROD001','Martillo de Uña','Martillo de acero con mango de madera',1,50,5.00,8.00,1,'2024-12-13 23:08:10',8),(32,'PROD002','Destornillador Plano','Destornillador de punta plana',2,102,2.50,4.00,1,'2024-12-13 23:08:10',8),(33,'PROD003','Llave Inglesa','Llave ajustable de 8 pulgadas',3,30,10.00,15.00,1,'2024-12-13 23:08:10',8),(34,'PROD004','Taladro Eléctrico','Taladro de 500W con brocas incluidas',4,30,50.00,75.00,1,'2024-12-13 23:08:10',18),(35,'PROD005','Cinta Métrica','Cinta métrica de 5 metros',5,70,3.00,5.00,1,'2024-12-13 23:08:10',1),(36,'PROD006','Sierra Manual','Sierra de mano para madera',6,20,8.00,12.00,1,'2024-12-13 23:08:10',8),(37,'PROD007','Tornillos 1 pulgada','Paquete de 100 tornillos de acero',7,100,3.50,5.50,1,'2024-12-13 23:08:10',9),(38,'PROD008','Clavos de 2 pulgadas','Paquete de 200 clavos galvanizados',7,200,4.00,6.50,1,'2024-12-13 23:08:10',9),(39,'PROD009','Pegamento Epóxico','Resina epóxica de 50 ml',8,60,2.00,3.50,1,'2024-12-13 23:08:10',5),(40,'PROD010','Pintura Blanca','Galón de pintura blanca mate',9,30,12.00,18.00,1,'2024-12-13 23:08:10',12),(41,'PROD011','Brocha 2 pulgadas','Brocha para pintar superficies lisas',9,80,1.50,3.00,1,'2024-12-13 23:08:10',8),(42,'PROD012','Rodillo de Pintura','Rodillo para pintura de paredes',9,50,4.00,6.50,1,'2024-12-13 23:08:10',8),(43,'PROD013','Cemento Portland','Bolsa de cemento de 50 kg',10,100,6.00,10.00,1,'2024-12-13 23:08:10',6),(44,'PROD014','Arena Fina','Metro cúbico de arena fina',10,20,15.00,25.00,1,'2024-12-13 23:08:10',14),(45,'PROD015','Tubería PVC 1 pulgada','Tubería de PVC para agua',11,60,1.20,2.00,1,'2024-12-13 23:08:10',1),(46,'PROD016','Codo PVC 90°','Codo de PVC para unión de tuberías',11,100,0.50,1.00,1,'2024-12-13 23:08:10',8),(47,'PROD017','Lija Grano Fino','Hoja de lija de 200 granos',12,200,0.30,0.60,1,'2024-12-13 23:08:10',8),(48,'PROD018','Masilla Plástica','Bote de masilla para reparación',12,50,3.00,5.00,1,'2024-12-13 23:08:10',5),(49,'PROD019','Cerradura de Puerta','Cerradura con llave y perilla',13,30,12.00,20.00,1,'2024-12-13 23:08:10',8),(50,'PROD020','Bisagra de Acero','Juego de bisagras para puertas',13,70,2.50,4.50,1,'2024-12-13 23:08:10',8),(51,'PROD021','Cable Eléctrico 2x16','Rollo de 100 metros',14,10,25.00,35.00,1,'2024-12-13 23:08:10',1),(52,'PROD022','Interruptor Unipolar','Interruptor eléctrico de pared',14,80,1.50,2.50,1,'2024-12-13 23:08:10',8),(53,'PROD023','Bombillo LED 9W','Bombillo LED de luz blanca',15,100,2.00,3.50,1,'2024-12-13 23:08:10',8),(54,'PROD024','Extensión Eléctrica','Extensión de 5 metros',15,50,6.00,10.00,1,'2024-12-13 23:08:10',1),(55,'PROD025','Guantes de Trabajo','Guantes de seguridad talla M',16,60,2.50,4.00,1,'2024-12-13 23:08:10',8),(56,'PROD026','Casco de Seguridad','Casco de seguridad para construcción',16,20,8.00,12.00,1,'2024-12-13 23:08:10',8),(57,'PROD027','Mascarilla Antipolvo','Paquete de 10 mascarillas',16,100,1.50,3.00,1,'2024-12-13 23:08:10',8),(58,'PROD028','Escalera de Aluminio','Escalera de 6 peldaños',17,15,35.00,50.00,1,'2024-12-13 23:08:10',1),(59,'PROD029','Carretilla','Carretilla metálica reforzada',17,5,40.00,65.00,1,'2024-12-13 23:08:10',1),(60,'PROD030','Nivel de Burbuja','Nivel de burbuja de 60 cm',18,30,6.00,9.00,1,'2024-12-13 23:08:10',8);
 /*!40000 ALTER TABLE `producto` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -336,7 +345,7 @@ CREATE TABLE `venta` (
   KEY `fk_venta_cliente` (`IdCliente`),
   CONSTRAINT `fk_venta_cliente` FOREIGN KEY (`IdCliente`) REFERENCES `cliente` (`IdCliente`),
   CONSTRAINT `fk_venta_usuario` FOREIGN KEY (`IdUsuario`) REFERENCES `usuario` (`IdUsuario`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -345,6 +354,7 @@ CREATE TABLE `venta` (
 
 LOCK TABLES `venta` WRITE;
 /*!40000 ALTER TABLE `venta` DISABLE KEYS */;
+INSERT INTO `venta` VALUES (2,5,13,300.00,60.00,240.00,'2024-12-26 19:00:40'),(3,5,4,300.00,25.00,275.00,'2024-12-26 19:01:57');
 /*!40000 ALTER TABLE `venta` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -390,6 +400,36 @@ BEGIN
         AND c.FechaRegistro BETWEEN p_FechaInicio AND p_FechaFin
     ORDER BY 
         c.FechaRegistro DESC;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `DetallesVenta` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `DetallesVenta`(IN p_IdVenta INT)
+BEGIN
+    SELECT 
+        dv.IdProducto, 
+        p.Nombre AS Producto, 
+        dv.cantidad, 
+        dv.PrecioVenta, 
+        (dv.Cantidad * dv.PrecioVenta) AS Total
+    FROM 
+        detalle_venta dv
+    JOIN 
+        producto p ON dv.IdProducto = p.IdProducto
+    WHERE 
+        dv.IdVenta = p_IdVenta;
 END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
@@ -564,7 +604,7 @@ BEGIN
     WHERE 
         DATE(v.FechaRegistro) = CURDATE()
     ORDER BY 
-        v.FechaRegistro, v.IdVenta, dv.IdDetalleVenta;
+        v.FechaRegistro desc, v.IdVenta desc, dv.IdDetalleVenta;
 END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
@@ -661,4 +701,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-12-26 17:03:59
+-- Dump completed on 2024-12-26 23:08:12
